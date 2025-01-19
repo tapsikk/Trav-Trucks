d86880facceacddf6dcbac841de2ed3306dc2354
tapsikk <tapsgame@gmail.com>
2025-01-19 23:03:04 +0200

    new link for title name

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,27 +4,28 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.brand}>
-        Travel<span className={styles.brandSub}>Trucks</span>
+      <NavLink to="/" className={styles.brand}>
+        Trav
+        <span className={styles.brandSub}>Trucks</span>
+      </NavLink>
+      <div className={styles.navLinks}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? styles.activeNavLink : styles.navLink
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) =>
+            isActive ? styles.activeNavLink : styles.navLink
+          }
+        >
+          Catalog
+        </NavLink>
       </div>
-        <div className={styles.navLinks}>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? styles.activeNavLink : styles.navLink
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/catalog"
-            className={({ isActive }) =>
-              isActive ? styles.activeNavLink : styles.navLink
-            }
-          >
-            Catalog
-          </NavLink>
-        </div>
     </nav>
   );
 };
