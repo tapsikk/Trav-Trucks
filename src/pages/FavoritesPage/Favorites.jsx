7bc4654eb8da7dc6bd831a31513d3b50aa5f0c64
tapsikk <tapsgame@gmail.com>
2025-02-01 00:57:30 +0200

    fix favorite file

--- src/pages/FavoritesPage/Favorites.jsx
+++ src/pages/FavoritesPage/Favorites.jsx
@@ -1,13 +1,9 @@
-import { useSelector } from "react-redux";
 import CampersList from "../../components/CampersList/CampersList";
-import { selectAllFavCampers } from "../../redux/user/selectors";
-// import styles from "./Favorites.module.css";
 import Filter from "../../components/Filter/Filter";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "../MainPage/MainPage.module.css";
 
 const Favorites = () => {
-  const favCampers = useSelector(selectAllFavCampers);
 
   const [filters, setFilters] = useState({
     location: "",
